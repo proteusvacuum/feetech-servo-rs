e539e6459d07135da90b373280d826cf277517ce
Farid Rener <proteusvacuum@gmail.com>
2025-03-21 15:38:14 -0400

    Unload motors on ctrl-c

--- src/driver.rs
+++ src/driver.rs
@@ -2,13 +2,15 @@ use crate::{commands::Command, packet_handler::PacketHandler};
 
 pub struct Driver {
     packet_handler: PacketHandler,
+    motor_ids: Vec<u8>,
 }
 
 impl Driver {
-    pub fn new(port_name: &str) -> Self {
+    pub fn new(port_name: &str, motor_ids: &[u8]) -> Self {
         let baud_rate = 1_000_000;
         Self {
             packet_handler: PacketHandler::new(port_name, baud_rate),
+            motor_ids: motor_ids.to_vec(),
         }
     }
 
@@ -19,5 +21,12 @@ impl Driver {
             _ => return None,
         };
         Some(status_packet.extract_data())
+    }
+
+    pub fn cleanup(&mut self) {
+        for motor_id in self.motor_ids.clone() {
+            self.act(motor_id, Command::WriteTorqueSwitch(false))
+                .unwrap();
+        }
     }
 }
